0dfb304ca3dade8b8f925c27b53c58426bdc3283
qiwip <iasnajder@gmail.com>
2022-12-02 17:15:47 +0300

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+def line(string):
+    vowels = ['у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю']
+ #   newline = string.lower()
+    total = 0
+    for s in string:
+        if s.lower() in vowels:
+            total += 1
+    return total
+print(line("здраФАствуйте"))
